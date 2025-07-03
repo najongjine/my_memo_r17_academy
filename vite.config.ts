471a5e721aa400e27e661c4a22cceb260e0c39e6
najongjine <najongjin3@hotmail.com>
2025-07-03 11:43:47 +0900

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/my_memo_r17_academy/", // 반드시 저장소 이름으로 맞춰주세요
+});
